b38a85b8a38f84ed1a3b054c812b412120ec33c4
dhairyashild <dhairyashild300@gmail.com>
2025-10-07 20:50:44 +0530

    Create WEBSITE.sh

---
+++ WEBSITE.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ${USER}
+docker --version
+
+git clone https://github.com/dhairyashild/java-source-code.git
+cd java-source-code
+docker build -t web .
+docker run -d -p 80:8080 web 
